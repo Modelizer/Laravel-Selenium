0084428475598bc17c300c0c217fca649bb5ef4e
Mohammed Mudasir <md.hyphen@gmail.com>
2016-09-18 17:54:14 +0530

    URL issue fixed and selenium url added in config.

--- src/config/drivers.php
+++ src/config/drivers.php
@@ -8,14 +8,16 @@ return [
         'mac' => [
             'version'  => '2.24',
             'url'      => 'http://chromedriver.storage.googleapis.com/2.24/chromedriver_mac64.zip',
-            'url'      => 'http://l52.dev/chromedriver_mac64.zip',
             'filename' => 'chromedriver'
         ],
         'win' => [
-            'version' => '2.24',
-//            'url'     => 'http://chromedriver.storage.googleapis.com/2.24/chromedriver_win32.zip',
-            'url'      => 'http://l52.dev:8000/chromedriver_win32.zip',
+            'version'  => '2.24',
+            'url'      => 'http://chromedriver.storage.googleapis.com/2.24/chromedriver_win32.zip',
             'filename' => 'chromedriver.exe'
         ],
     ],
+    'selenium' => [
+        'version' => '2.53.1',
+        'url'     => 'http://selenium-release.storage.googleapis.com/2.53/selenium-java-2.53.1.zip',
+    ]
 ];
