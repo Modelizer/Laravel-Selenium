6a96329054388cefd513b7b1cb577085cfef5c07
Christopher Pecoraro <chrispecoraro@gmail.com>
2016-10-30 14:51:43 +0100

    adding select()

--- src/Services/InteractWithPage.php
+++ src/Services/InteractWithPage.php
@@ -38,6 +38,21 @@ trait InteractWithPage
         return $this;
     }
 
+
+    /**
+     * "Select" a radio button or drop-down field.
+     *
+     * @param $value
+     * @param $elementName
+     */
+    protected function select($value, $elementName)
+    {
+        $element = $this->findElement($elementName);
+        $this->select($element)->selectOptionByValue($value);
+
+        return $this;
+    }
+
     /**
      * Assert that we see text within the specified tag
      * Defaults to the body tag.
@@ -55,6 +70,8 @@ trait InteractWithPage
     }
 
     /**
+     * User should not be able to see element.
+     *
      * @param $text
      * @param string $tag
      */
