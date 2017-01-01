6abdd55b78adc015b2046348f309359b80539788
Damian.K <damian.k@empiredatagroup.com>
2017-01-01 11:34:05 +0100

    Style changes

--- src/TestingMiddleware.php
+++ src/TestingMiddleware.php
@@ -2,21 +2,14 @@
 
 namespace Modelizer\Selenium;
 
-use Closure;
 use App;
+use Closure;
 
 class TestingMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
     public function handle($request, Closure $next)
     {
-        if(App::environment('testing')){
+        if (App::environment('testing')) {
             config(['database.default' => 'testing']);
         }
 
