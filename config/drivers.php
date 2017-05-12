9643524dc2a7f88ea3a723d0eff06b160e2bcd70
Drew Budwin <drew@budw.in>
2017-05-12 16:17:52 -0400

    Updated the config for the driver versions and URLs

--- config/drivers.php
+++ config/drivers.php
@@ -6,24 +6,24 @@
 return [
     'chrome' => [
         'mac' => [
-            'version'  => '2.24',
-            'url'      => 'http://chromedriver.storage.googleapis.com/2.24/chromedriver_mac64.zip',
+            'version'  => '2.29',
+            'url'      => 'https://chromedriver.storage.googleapis.com/2.29/chromedriver_mac64.zip',
             'filename' => 'chromedriver',
         ],
         'win' => [
-            'version'  => '2.24',
-            'url'      => 'http://chromedriver.storage.googleapis.com/2.24/chromedriver_win32.zip',
+            'version'  => '2.29',
+            'url'      => 'https://chromedriver.storage.googleapis.com/2.29/chromedriver_win32.zip',
             'filename' => 'chromedriver.exe',
         ],
         'linux' => [
-            'version'  => '2.24',
-            'url'      => 'http://chromedriver.storage.googleapis.com/2.24/chromedriver_linux64.zip',
+            'version'  => '2.29',
+            'url'      => 'https://chromedriver.storage.googleapis.com/2.29/chromedriver_linux64.zip',
             'filename' => 'chromedriver',
         ],
     ],
     'firefox'  => [],
     'selenium' => [
-        'version' => '2.53.1',
-        'url'     => 'http://selenium-release.storage.googleapis.com/2.53/selenium-java-2.53.1.zip',
+        'version' => '3.4.0',
+        'url'     => 'https://selenium-release.storage.googleapis.com/3.4/selenium-server-standalone-3.4.0.jar',
     ],
 ];
