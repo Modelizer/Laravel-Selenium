d96157d16557a2244603a3195b79238aff76154d
Mohammed Mudasir <md.hyphen@gmail.com>
2018-04-04 07:22:16 +0300

    Loading testing.env file and some more clean up.

--- storage/views/a581172ace3238a663547ff05a20698e70b3185e.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('title', 'Page Not Found'); ?>
-
-<?php $__env->startSection('message', 'Sorry, the page you are looking for could not be found.'); ?>
-
-<?php echo $__env->make('errors::layout', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
